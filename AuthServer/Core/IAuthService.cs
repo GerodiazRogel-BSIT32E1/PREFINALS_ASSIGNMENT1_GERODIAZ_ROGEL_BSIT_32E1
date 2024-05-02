3f340dd5c70ae469b8a9c358196e570237476f65
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-05-02 14:00:24 +0800

    for the bonus

--- AuthServer/Core/IAuthService.cs
+++ AuthServer/Core/IAuthService.cs
@@ -1,4 +1,31 @@
-public interface IAuthService
+public class AuthService : IAuthService
 {
-    Task<string> GenerateJwtToken(User user);
+    private readonly IConfiguration _configuration;
+
+    public AuthService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<string> GenerateJwtToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+
+            new Claim(ClaimTypes.Role, "Admin")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtSettings:Issuer"],
+            audience: _configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+        return await Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+    }
 }
