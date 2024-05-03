42d9b2b057bf7ffa7558a525dbdfd6e93f1b0918
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-05-03 18:08:58 +0800

    make the username and password required

--- AuthServer/Core/User.cs
+++ AuthServer/Core/User.cs
@@ -4,8 +4,8 @@ namespace AuthServer.Core
 {
     public class User
     {
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
 
     }
 }
